6797f3c01136e7b204129b0e05f32e75fd7fcd4a
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:10:09 -0500

    Update index.php

--- PUNTO2/index.php
+++ PUNTO2/index.php
@@ -1,1 +1,11 @@
+<?php
+
+// 1. Cargar helper de vistas
+require_once __DIR__ . '/core/helpers.php';
+
+// 2. Cargar controladores
+require_once __DIR__ . '/controllers/TipController.php';
+
+// 3. Cargar rutas
+require_once __DIR__ . '/routes/web.php';
 
