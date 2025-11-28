deaec25991a98798f9979715011c6fd351f71fb2
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:42:11 -0500

    Update web.php

--- PUNTO8/routes/web.php
+++ PUNTO8/routes/web.php
@@ -1,1 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecetaController;
+
+Route::get('/', [RecetaController::class, 'index'])->name('home');
+Route::post('/recetas/agregar', [RecetaController::class, 'agregar'])->name('recetas.agregar');
+Route::get('/recetas/buscar', [RecetaController::class, 'buscar'])->name('recetas.buscar');
 
