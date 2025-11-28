75a58d469686eac550d0269154a552c4c2feca5f
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:31:40 -0500

    Update ReservaController.php

--- PUNTO5/app/Http/Controllers/ReservaController.php
+++ PUNTO5/app/Http/Controllers/ReservaController.php
@@ -1,1 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReservaController extends Controller
+{
+    // Array para almacenar reservas temporalmente (simula base de datos)
+    private $reservas = [];
+
+    // Muestra la vista principal
+    public function index()
+    {
+        return view('welcome', ['reservas' => $this->reservas]);
+    }
+
+    // Registra una nueva reserva
+    public function registrar(Request $request)
+    {
+        $reserva = [
+            'fecha' => $request->input('fecha'),
+            'hora' => $request->input('hora'),
+            'servicio' => $request->input('servicio'),
+            'nombre' => $request->input('nombre'),
+        ];
+
+        // Guardamos temporalmente en el array
+        $this->reservas[] = $reserva;
+
+        return view('welcome', ['reservas' => $this->reservas]);
+    }
+}
 
