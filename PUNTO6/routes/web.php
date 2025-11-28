d1092abcd65de048c686b1f0c2b4b7d0fc4b397e
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:35:16 -0500

    Update web.php

--- PUNTO6/routes/web.php
+++ PUNTO6/routes/web.php
@@ -1,1 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NotaController;
+
+Route::get('/', [NotaController::class, 'index'])->name('home');
+Route::post('/notas/registrar', [NotaController::class, 'registrar'])->name('notas.registrar');
 
