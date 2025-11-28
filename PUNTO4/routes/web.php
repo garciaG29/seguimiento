ad812732d39c028ca66a2f0e635765084424fef0
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:25:54 -0500

    Update web.php

--- PUNTO4/routes/web.php
+++ PUNTO4/routes/web.php
@@ -1,1 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GastoController;
+
+Route::get('/', [GastoController::class, 'index'])->name('home');
+Route::post('/gastos/registrar', [GastoController::class, 'registrar'])->name('gastos.registrar');
 
