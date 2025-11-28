23783473c8e0e64db27271cc8fe36fa1b4d0e9ba
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-27 23:02:43 -0500

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Proyecto Laravel - Ejercicios MVC</title>
+</head>
+<body>
+    <h1>Ejercicios MVC - Proyecto en desarrollo</h1>
 
+    <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/tareas">Lista de Tareas</a></li>
+        <li><a href="/propinas">Calculadora Propinas</a></li>
+        <li><a href="/password">Generar Contraseña</a></li>
+    </ul>
+
+    <hr>
+
+    <!-- Aquí se insertará contenido dinámico -->
+    @yield('content')
+
+</body>
+</html>
