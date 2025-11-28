054df0d8d9d177348b08ba5a33ca29d56dbf1ba0
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:31:22 -0500

    Update web.php

--- PUNTO5/routes/web.php
+++ PUNTO5/routes/web.php
@@ -1,1 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReservaController;
+
+Route::get('/', [ReservaController::class, 'index'])->name('home');
+Route::post('/reservas/registrar', [ReservaController::class, 'registrar'])->name('reservas.registrar');
 
