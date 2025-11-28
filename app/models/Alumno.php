6f3d6f10de993380f7179160a18128be882a83ac
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 00:40:01 -0500

    Update Alumno.php

--- app/models/Alumno.php
+++ app/models/Alumno.php
@@ -1,12 +1,23 @@
 <?php
 
 class Alumno {
-    public $nombre;
-    public $edad;
 
-    public function __construct($nombre = "", $edad = "") {
+    private $nombre;
+    private $curso;
+    private $grado;
+    private $telefono;
+
+    public function __construct($nombre, $curso, $grado, $telefono) {
         $this->nombre = $nombre;
-        $this->edad = $edad;
+        $this->curso = $curso;
+        $this->grado = $grado;
+        $this->telefono = $telefono;
+    }
+
+    // Método para devolver la información lista para guardar en TXT
+    public function formatoTexto() {
+        return "Nombre: {$this->nombre} - Curso: {$this->curso} - Grado: {$this->grado} - Teléfono: {$this->telefono}\n";
     }
 }
+
 ?>
