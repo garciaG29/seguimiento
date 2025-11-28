5968ad15d57b4a0828bcb73dfd84e4661d6e2362
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:38:38 -0500

    Update web.php

--- PUNTO7/routes/web.php
+++ PUNTO7/routes/web.php
@@ -1,1 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+
+Route::get('/', [EventoController::class, 'index'])->name('home');
+Route::post('/eventos/agregar', [EventoController::class, 'agregar'])->name('eventos.agregar');
+Route::post('/eventos/editar', [EventoController::class, 'editar'])->name('eventos.editar');
+Route::post('/eventos/eliminar', [EventoController::class, 'eliminar'])->name('eventos.eliminar');
 
