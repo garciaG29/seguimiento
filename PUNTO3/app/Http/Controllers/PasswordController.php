53ead827a7db54c2308f7baa20b6f06940050de3
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:21:53 -0500

    Update PasswordController.php

--- PUNTO3/app/Http/Controllers/PasswordController.php
+++ PUNTO3/app/Http/Controllers/PasswordController.php
@@ -1,1 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PasswordController extends Controller
+{
+    // Muestra la vista principal
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    // Genera la contraseña
+    public function generar(Request $request)
+    {
+        $longitud = $request->input('longitud', 12); // por defecto 12
+        $mayusculas = $request->input('mayusculas', true);
+        $numeros = $request->input('numeros', true);
+        $simbolos = $request->input('simbolos', true);
+
+        $caracteres = 'abcdefghijklmnopqrstuvwxyz';
+        if($mayusculas) $caracteres .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if($numeros) $caracteres .= '0123456789';
+        if($simbolos) $caracteres .= '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+        $password = '';
+        $max = strlen($caracteres) - 1;
+        for ($i = 0; $i < $longitud; $i++) {
+            $password .= $caracteres[random_int(0, $max)];
+        }
+
+        return view('welcome', ['password' => $password]);
+    }
+}
 
