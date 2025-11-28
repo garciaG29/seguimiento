750608898b475dddfa2e95991d0f3a0afe93c0d5
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:54:58 -0500

    Update CronometroController.php

--- PUNTO11/app/Http/Controllers/CronometroController.php
+++ PUNTO11/app/Http/Controllers/CronometroController.php
@@ -1,1 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CronometroController extends Controller
+{
+    // Array temporal para almacenar vueltas
+    private $vueltas = [];
+
+    // Muestra la vista principal
+    public function index()
+    {
+        return view('welcome', ['vueltas' => $this->vueltas]);
+    }
+
+    // Guardar vuelta
+    public function guardarVuelta(Request $request)
+    {
+        $tiempo = $request->input('tiempo');
+        $this->vueltas[] = $tiempo;
+
+        return view('welcome', ['vueltas' => $this->vueltas]);
+    }
+}
 
