9a603b70c2cb6fec461d7b0417cb7b13a69b401e
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:50:21 -0500

    Update web.php

--- PUNTO10/routes/web.php
+++ PUNTO10/routes/web.php
@@ -1,1 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EncuestaController;
+
+Route::get('/', [EncuestaController::class, 'index'])->name('home');
+Route::post('/encuestas/crear', [EncuestaController::class, 'crear'])->name('encuestas.crear');
+Route::post('/encuestas/responder', [EncuestaController::class, 'responder'])->name('encuestas.responder');
+Route::get('/encuestas/resultados', [EncuestaController::class, 'resultados'])->name('encuestas.resultados');
 
