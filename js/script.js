43567e63c4218b3c87be75c77b51b9a5c1877159
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 00:04:49 -0500

    Delete js directory

--- js/script.js
+++
@@ -1,5 +0,0 @@
-// Punto 6: JavaScript básico de interacción
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("JavaScript cargado correctamente");
-    alert("Bienvenido al formulario!");
-});
