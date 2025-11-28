c7013c17542e5473c5689344bc6111f6925aa8c1
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:54:37 -0500

    Update web.php

--- PUNTO11/routes/web.php
+++ PUNTO11/routes/web.php
@@ -1,1 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CronometroController;
+
+Route::get('/', [CronometroController::class, 'index'])->name('home');
+Route::post('/cronometro/guardar-vuelta', [CronometroController::class, 'guardarVuelta'])->name('cronometro.guardarVuelta');
 
