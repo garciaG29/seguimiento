be6c53952554252c085ce379f2047edf9623e03f
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:45:42 -0500

    Update web.php

--- PUNTO9/routes/web.php
+++ PUNTO9/routes/web.php
@@ -1,1 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MemoriaController;
+
+Route::get('/', [MemoriaController::class, 'index'])->name('home');
+Route::post('/juego/iniciar', [MemoriaController::class, 'iniciar'])->name('juego.iniciar');
+Route::post('/juego/guardar-puntaje', [MemoriaController::class, 'guardarPuntaje'])->name('juego.guardarPuntaje');
 
