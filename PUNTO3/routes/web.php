c1a5c20c5d8b3f9dee43b80d4adaf75c0813c42a
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:21:32 -0500

    Update web.php

--- PUNTO3/routes/web.php
+++ PUNTO3/routes/web.php
@@ -1,1 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PasswordController;
+
+Route::get('/', [PasswordController::class, 'index'])->name('home');
+Route::post('/generar', [PasswordController::class, 'generar'])->name('generar');
 
