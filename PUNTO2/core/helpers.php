3352aa9c6e0546ccd72589216aa0f2ad57ca6c3a
garciaG29 <139415851+garciaG29@users.noreply.github.com>
2025-11-28 01:05:11 -0500

    Update helpers.php

--- PUNTO2/core/helpers.php
+++ PUNTO2/core/helpers.php
@@ -1,1 +1,15 @@
+<?php
+
+function view($name, $data = [])
+{
+    $path = __DIR__ . '/../views/' . $name . '.php';
+
+    if (!file_exists($path)) {
+        echo "La vista $name no existe.";
+        return;
+    }
+
+    extract($data);
+    include $path;
+}
 
